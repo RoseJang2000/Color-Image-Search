b9acc521b7f196949fd99bd2e5a057f7093d9391
Rose Jang <jangmi749@gmail.com>
2023-03-31 16:08:46 +0900

    :recycle: refactor : 무한스크롤 옵저버 방식으로 변경

--- src/components/ImageItemList.tsx
+++ src/components/ImageItemList.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from 'react';
-import ImageItem from 'components/ImageItem';
-import axios from 'axios';
+import { useEffect, useRef, useState } from 'react';
+import ImageItem from './ImageItem';
 import './ImageItemList.css';
-import TopButton from 'components/TopButton';
+import TopButton from './TopButton';
 import Masonry from 'react-masonry-css';
 import {
   ImageItemListProps,
@@ -14,7 +13,10 @@ import Kakao from 'api/Kakao';
 const ImageItemList = ({ keyword, page, setPage }: ImageItemListProps) => {
   const [prevKeyword, setPrevKeyword] = useState<string>(keyword);
   const [datas, setDatas] = useState<ResponseData[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const preventRef = useRef(true); // ! 무한스크롤 중복 실행 방지
+  const observerRef = useRef(null); // ! 무한스크롤 옵저버 엘리먼트용
+  const [isLastPage, setIsLastPage] = useState<boolean>(false);
 
   // * 화면 크기에 따라 layout column 숫자 변경
   const changeGap = () => {
@@ -33,10 +35,9 @@ const ImageItemList = ({ keyword, page, setPage }: ImageItemListProps) => {
     setGap(() => changeGap());
   };
 
-  const [gap, setGap] = useState<number>(() => changeGap());
+  const [gap, setGap] = useState(() => changeGap());
 
   // ! 카카오 검색 API 데이터 가져오기
-
   const imageSearch = (params: RequestParams) => {
     return Kakao.get('/v2/search/image', { params });
   };
@@ -48,7 +49,11 @@ const ImageItemList = ({ keyword, page, setPage }: ImageItemListProps) => {
       size: 20,
     };
     const { data } = await imageSearch(params);
-
+    if (data.meta.is_end) {
+      // * API 데이터 마지막 페이지일 경우
+      setIsLastPage(true);
+    }
+    preventRef.current = true;
     setDatas((prev) => [...prev, ...data.documents]);
     setIsLoading(false);
   };
@@ -64,23 +69,30 @@ const ImageItemList = ({ keyword, page, setPage }: ImageItemListProps) => {
   }, [keyword, page]);
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
     window.addEventListener('resize', windowResize);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
       window.addEventListener('resize', windowResize);
     };
   });
 
-  const handleScroll = () => {
-    const scrollHeight = Math.ceil(document.documentElement.scrollHeight);
-    const scrollTop = Math.ceil(document.documentElement.scrollTop);
-    const clientHeight = Math.ceil(document.documentElement.clientHeight);
+  const obsHandler = (entries: IntersectionObserverEntry[]) => {
+    const target = entries[0];
 
-    if (scrollTop + clientHeight >= scrollHeight) {
-      setPage((prev) => prev + 1);
+    if (!isLastPage && target.isIntersecting && preventRef.current) {
+      // ! 옵저버를 만났을 때 마지막 페이지가 아니고, 중복 실행이 아닐 경우
+      preventRef.current = false; // * 옵저버 중복 실행 방지
+      setPage((prev) => prev + 1); // * 페이지 값 증가
     }
   };
+
+  useEffect(() => {
+    // ! 옵저버 핸들러 호출 (종속성배열: 검색 관련 데이터로 바뀔때 재시동)
+    const observer = new IntersectionObserver(obsHandler, { threshold: 0.5 });
+    if (observerRef.current) observer.observe(observerRef.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [keyword]);
 
   return (
     <main>
@@ -100,6 +112,7 @@ const ImageItemList = ({ keyword, page, setPage }: ImageItemListProps) => {
         )}
         <TopButton />
       </section>
+      <div ref={observerRef}>{/* Observer */}</div>
     </main>
   );
 };
