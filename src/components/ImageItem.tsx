22af62662bee77835f946fef75f5f37165176e61
Rose Jang <jangmi749@gmail.com>
2023-03-31 16:18:02 +0900

    :recycle: refactor : 에러이미지 import 방식으로 수정

--- src/components/ImageItem.tsx
+++ src/components/ImageItem.tsx
@@ -1,11 +1,11 @@
 import { ResponseData } from 'interfaces/AllTypes.interfaces';
 import { SyntheticEvent } from 'react';
+import noImg from 'assets/no-image.png';
 
 const ImageItem = ({ data }: { data: ResponseData }) => {
   /** 이미지 src 에러 발생 시 오류 이미지로 변경하는 함수 (엑스박스 방지) */
   const onErrorImg = (event: SyntheticEvent<HTMLImageElement>) => {
-    (event.target as HTMLImageElement).src =
-      process.env.PUBLIC_URL + '/no-image.png';
+    (event.target as HTMLImageElement).src = noImg;
   };
 
   return (
